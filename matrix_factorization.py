899bde1e3da61073693db90cb215ec885bb3f177
brooke-zhou <56696011+brooke-zhou@users.noreply.github.com>
2020-02-26 06:39:56 -0800

    Update matrix_factorization.py

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -13,21 +13,41 @@ import matplotlib.pyplot as plt
 import data_cleaning as cleaner
 import SurpriseMatrixFactorization as sp
 
-# import original data and clean
-path_to_original_movies_file = '../data/movies.txt'
-path_to_original_data='../data/data.txt'
-movies, duplicate_count, replace_table = \
-    cleaner.clean_movies(path_to_original_movies_file, save=True)
-data = cleaner.clean_data(replace_table, path_to_original_data, save_new_data='txt')
+# # import original data and clean (only need to run once if you store the cleaned files)
+# path_to_original_movies_file = '../data/movies.txt'
+# path_to_original_data='../data/data.txt'
+# movies, duplicate_count, replace_table = \
+#     cleaner.clean_movies(path_to_original_movies_file, save=True)
+# data = cleaner.clean_data(replace_table, path_to_original_data, save_new_data='txt')
+# path_to_train_data='../data/train.txt'
+# train_data = cleaner.clean_data(replace_table, path_to_train_data, save_new_data='txt')
+# path_to_test_data='../data/test.txt'
+# test_data = cleaner.clean_data(replace_table, path_to_test_data, save_new_data='txt')
+
+path_to_test_data='../data/test_clean.txt'
+test_data = np.loadtxt(path_to_test_data)
 
 ##############
 # biased SVD #
 ##############
 
 # Step 1: learn U and V by Matrix Factorization (Y ~ U^T V)
-V, U = sp.surpriseSVD(DataPath = '../data/data_clean.txt')
+V, U, _, _ = sp.surpriseSVD(DataPath = '../data/data_clean.txt',
+                            n_factors=100, n_epochs=10, 
+                            lr_all=0.005, reg_all=0.02)
 
-# TO DO Step 2:evaluate test error
+# Step 2:evaluate test error
+test_err = 0
+# UT = U.transpose()
+for line in test_data:
+    user = int(line[0])
+    movie = int(line[1])
+    Y_test = line[2]
+    UTi = U[user-1]
+    Vj = V[movie-1]
+    Y_pred = np.dot(UTi,Vj)
+    test_err += (Y_pred - Y_test)**2
+
 
 # TO DO Step 3: project U, V into a 2D space: (V = A Sigma B)
 
